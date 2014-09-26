dd9bc62a8f68d5e94bd8cf5fc516e0ec62fc1fef
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-26 16:08:07 +0100

    Add simple index page, serve it

---
+++ index.js
@@ -0,0 +1,6 @@
+var webServer = require('./server').server;
+
+webServer.listen(7000);
+
+console.log('Server started');
+
---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http'),
+    path = require('path'),
+    fs = require('fs');
+
+var server = http.createServer(function onRequest(request, response) {
+    console.log('requesting page');
+    var indexPath = path.join(process.cwd(), 'html/index.html')
+    fs.readFile(indexPath, 'binary', function readFile(err, file) {
+        if (err) {
+            response.writeHeader(500, {'Content-Type': 'text/plain'});
+            response.write(err + '\n');
+            response.end();
+        } else {
+            response.writeHeader(200, {'Content-Type': 'text/html'});
+            response.write(file, 'binary');
+            response.end();
+        }
+    });
+});
+
+exports.server = server;
+
+
