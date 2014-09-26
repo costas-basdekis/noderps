73abb67bd9b769b166e86e3b01474048fba52860
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-26 17:25:57 +0100

    Add in-memory database

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
+console.log('New DB');
 db.serialize(function serialize() {
-    db.run("CREATE TABLE users (id LONG, token LONG, name TEXT)");
+    console.log('Creating DB');
+    db.run("CREATE TABLE IF NOT EXISTS users (id LONG, token LONG, name TEXT)");
 });
 
 exports.db = db;
