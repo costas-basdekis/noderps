7cd889e2c90f8b0fc5885e4a734afc0a7336b6aa
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-27 14:03:36 +0100

    Make all DB queries be serialized

--- db.js
+++ db.js
@@ -2,10 +2,13 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
 console.log('New DB');
-db.serialize(function serialize() {
+db.serialize();
+function createDB() {
     console.log('Creating DB');
     db.run("CREATE TABLE IF NOT EXISTS users (id LONG, token LONG, name TEXT, score LONG, state TEXT, choice TEXT)");
-});
+}
+
+createDB();
 
 exports.db = db;
 
