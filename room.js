b4f64cd096b980db695c22dfdef3f2d9709824b9
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-27 11:05:28 +0100

    Refactor websocket server
    - Check for too big message
    - Check for binary message
    - Check that the action is a defined action in handlers

--- room.js
+++ room.js
@@ -8,22 +8,46 @@ var wss = new WebSocketServer({port: 7001});
 utils.init(wss);
 
 wss.handlers = handlers;
-wss.on('connection', function onConnection(socket) {
-    console.log('new connection');
-    socket.on('message', function socketOnMessage(data, flags) {
-        console.log('received: %s', data);
-        try {
-            data = JSON.parse(data)
-        } catch (e) {
-            return;
-        }
+function extractMessage(data, flags) {
+    if (flags.binary) {
+        return {error: 'binary messages are not supported'};
+    }
+    if (data.length > 1000) {
+        return {error: 'message was too big: ' + data.length};
+    }
+    console.log('received: %s', data);
+    try {
+        message = JSON.parse(data);
+    } catch (e) {
+        return {error: 'message was not JSON'};
+    }
 
-        if (data.action in wss.handlers) {
-            wss.handlers[data.action](socket, data);
-        } else {
-            console.log('unknown action %s', data.action);
-        }
-    });
+    return {message: message};
+}
+function socketOnMessage(data, flags) {
+    var extracted = extractMessage(data, flags);
+    if (extracted.error) {
+        console.log(extracted.error);
+        return;
+    }
+    var message = extracted.message;
+
+    if (wss.handlers.hasOwnProperty(message.action)) {
+        wss.handlers[message.action](this, message);
+    } else {
+        console.log('unknown action %s', message.action);
+    }
+}
+
+wss.on('connection', function onConnection(socket) {
+    console.log('new connectioni, now %s clients', wss.clients.length);
+    if (wss.clients.length > 10) {
+        console.log('too many clients, dropping');
+        socket.close();
+        return;
+    }
+
+    socket.on('message', socketOnMessage.bind(socket));
 });
 
 console.log('Websocket server started');
