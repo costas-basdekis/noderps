b47d090772f40f439283d1e462b8c24f3f43838d
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-27 21:08:15 +0000

    Add post pull script

---
+++ deploy/post_pull.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /webapps/noderps/noderps
+
+# DEploy configuration
+cp deploy/nginx/noderps.conf /etc/nginx/sites-available/noderps.cfg
+ln -s /etc/nginx/sites-available/noderps.cfg /etc/nginx/sites-enabled/noderps.cfg
+cp deploy/haproxy/noderps.cfg /etc/haproxy/noderps.cfg
+
+# Restart services
+service nginx reload
+start-stop-daemon --stop --name haproxy
+haproxy -f /etc/haproxy/noderps.cfg -D
+forever stop room.js
+forever start room.js
+
