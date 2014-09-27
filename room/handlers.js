2f30475e4315b97743393ab9405b27c57df85b68
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-27 11:24:48 +0100

    Extract functions from handlers.js to utils.js

--- room/handlers.js
+++ room/handlers.js
@@ -3,85 +3,23 @@ var utils = require('./utils');
 
 var handlers = {
     join: function join(socket, data) {
-        console.log('new user');
-        var user = {
-            id: Math.ceil(Math.random() * 1000000),
-            token: Math.ceil(Math.random() * 1000000),
-            score: 0,
-            state: 'waiting',
-        };
-        user.name = data.name || 'User ' + user.id
+        var user = utils.addUser({name: data.name});
 
-        db.run("INSERT INTO users (id, token, name, score, state) VALUES "+
-               "($id, $token, $name, $score, $state)", {
-            $id: user.id,
-            $token: user.token,
-            $name: user.name,
-            $score: user.score,
-            $state: user.state,
-        });
-
-        socket.send(JSON.stringify({
+        utils.send(socket, {
             type: 'user_token',
             user: user,
-        }));
-        utils.broadcast(JSON.stringify({
-           type: 'new_user',
-           user: user,
-        }));
+        });
     },
     list_users: function list_users(socket, data) {
-        db.all("SELECT id, name, score, state FROM users",
-        function dbAll(err, rows) {
-            var users = rows.map(function(row) {
-                return {
-                    id: row.id,
-                    name: row.name,
-                    score: row.score,
-                    state: row.state,
-                };
-            });
-            socket.send(JSON.stringify({
+        utils.getUsers(function(users) {
+            utils.send(socket, {
                 type: 'list_users',
                 users: users
-            }));
+            });
         });
     },
     choose: function choose(socket, data) {
-        if (['rock', 'paper', 'sciscors'].indexOf(data.choice) == -1) {
-            console.log('invalid selction %s', data.choice);
-            return;
-        }
-
-        db.all("SELECT token, state FROM users WHERE token = $token",
-        {$token: data.token},
-        function dbAll(err, rows) {
-            if (err) {
-                console.log('error %s', err);
-                return;
-            } else if (rows.length == 0) {
-                console.log('unknown user made a choice %s', data.token);
-                return;
-            }
-            var row = rows[0];
-
-            db.run("UPDATE users SET choice = $choice, state = $state "+
-                   "WHERE token = $token", {
-                $token: data.token,
-                $choice: data.choice,
-                $state: 'ready',
-            });
-
-            db.get("SELECT COUNT(*) AS waiting FROM users WHERE state = " +
-                   "'waiting'",
-            function dbGet(err, row) {
-                if (row.waiting == 0) {
-                    console.log('all ready!');
-                } else {
-                    console.log('%s people waiting', row.waiting);
-                }
-            });
-        })
+        utils.makeChoice(data.token, data.choice);
     },
 };
 
--- room/utils.js
+++ room/utils.js
@@ -6,12 +6,116 @@ function init($wss) {
     wss = $wss;
 }
 
+function send(socket, data) {
+    var j = JSON.stringify(data);
+    socket.send(j);
+}
+
 function broadcast(data) {
+    var j = JSON.stringify(data);
     for (var i = 0, client ; client = wss.clients[i] ; i++) {
-        client.send(data);
+        client.send(j);
     }
 }
 
+function addUser(user) {
+    console.log('new user');
+    
+    user = {
+        id: Math.ceil(Math.random() * 1000000),
+        token: Math.ceil(Math.random() * 1000000),
+        score: 0,
+        state: 'waiting',
+        name: user.name,
+    };
+    user.name = user.name || 'User #' + user.id;
+
+    db.run("INSERT INTO users (id, token, name, score, state) VALUES "+
+           "($id, $token, $name, $score, $state)", {
+        $id: user.id,
+        $token: user.token,
+        $name: user.name,
+        $score: user.score,
+        $state: user.state,
+    });
+
+    broadcastNewUser(user);
+
+    return user;
+}
+
+function broadcastNewUser(user) {
+    broadcast({
+       type: 'new_user',
+       user: user,
+    });
+}
+
+function getUsers(callback) {
+    var users = [];
+    db.each("SELECT id, name, score, state FROM users",
+    function dbEach(err, row) {
+        users.push({
+            id: row.id,
+            name: row.name,
+            score: row.score,
+            state: row.state,
+        });
+    }, function dbEachComplete() {
+        callback(users);
+    });
+}
+
+CHOICES = ['rock', 'paper', 'sciscors'];
+
+function makeChoice(token, choice) {
+    if (CHOICES.indexOf(choice) == -1) {
+        console.log('invalid selction %s', choice);
+        return;
+    }
+
+    db.all("SELECT token, state FROM users WHERE token = $token",
+    {$token: token},
+    function dbAll(err, rows) {
+        if (err) {
+            console.log('error %s', err);
+            return;
+        } else if (rows.length == 0) {
+            console.log('unknown user made a choice %s', token);
+            return;
+        }
+
+        updateUserChoice(token, choice);
+    })
+}
+
+function updateUserChoice(token, choice) {
+    db.run("UPDATE users SET choice = $choice, state = $state "+
+           "WHERE token = $token", {
+        $token: token,
+        $choice: choice,
+        $state: 'ready',
+    });
+
+    checkAllUsersAreReady();
+}
+
+function checkAllUsersAreReady() {
+    db.get("SELECT COUNT(*) AS waiting FROM users WHERE state = " +
+           "'waiting'",
+    function dbGet(err, row) {
+        if (row.waiting == 0) {
+            console.log('all ready!');
+        } else {
+            console.log('%s people waiting', row.waiting);
+        }
+    });
+}
+
 exports.init = init;
+exports.send = send;
 exports.broadcast = broadcast;
+exports.addUser = addUser;
+exports.getUsers = getUsers;
+exports.makeChoice = makeChoice;
 
