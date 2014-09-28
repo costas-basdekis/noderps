cfbefecfda8b82f150bf74b795a8c80f1b1a93f5
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-28 13:18:23 +0100

    Allow local server to serve any file

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var http = require('http'),
+    url = require('url');
     path = require('path'),
     fs = require('fs');
 
+var staticRoot = './static';
+var indexFile = '/html/index.html';
+
 var server = http.createServer(function onRequest(request, response) {
-    console.log('requesting page');
-    var indexPath = path.join(process.cwd(), 'static/html/index.html')
+    var pathname = url.parse(request.url).pathname;
+    console.log('requesting page %s', pathname);
+
+    // Server index file for root
+    if (pathname == '/') {
+        pathname = indexFile;
+    }
+
+    var indexPath = path.join(process.cwd(), staticRoot, pathname);
     fs.readFile(indexPath, 'binary', function readFile(err, file) {
         if (err) {
-            response.writeHeader(500, {'Content-Type': 'text/plain'});
+            response.writeHeader(404, {'Content-Type': 'text/plain'});
             response.write(err + '\n');
             response.end();
         } else {
